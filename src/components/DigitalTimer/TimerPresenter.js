b128ca0386d6bf1ab30c1613513bc6041af15691
Kwon770 <sckwon770@gmail.com>
2020-03-12 17:26:53 +0900

    feat: Change all code of menu'

--- src/components/DigitalTimer/TimerPresenter.js
+++ src/components/DigitalTimer/TimerPresenter.js
@@ -11,41 +11,187 @@ import {
 import Store from "store";
 
 class TimerPresenter extends React.Component {
+  state = {
+    isFocus: false,
+    isMenuClick: false,
+    isCustomClick: false
+  };
   render() {
+    const { isMenuClick, isCustomClick } = this.state;
     return (
       <Container>
-        <MenuConatiner>
-          <CurrentDoPanel>
-            <FontAwesomeIcon icon={faPen} style={{ marginRight: 10 }} />
-            Code
-          </CurrentDoPanel>
-          <CogBtn>
-            <FontAwesomeIcon icon={faCog} />
-          </CogBtn>
-        </MenuConatiner>
-        <TimerContainer>
-          <DigitalTimer>25</DigitalTimer>
-          <DigitalTimer>
-            '<DigitalTimerDot>'</DigitalTimerDot>
-          </DigitalTimer>
-          <DigitalTimer>00</DigitalTimer>
-          <PlayerPreview>Soup Asmr - Tokyo Cafe Asmr</PlayerPreview>
-        </TimerContainer>
-        <TimerBtnContainer>
-          <TimerBtn>
-            <FontAwesomeIcon icon={faPlay} size="4x" color="white" />
-          </TimerBtn>
-          <TimerBtn>
-            <FontAwesomeIcon icon={faPause} size="4x" color="white" />
-          </TimerBtn>
-          <TimerBtn>
-            <FontAwesomeIcon icon={faStop} size="4x" color="white" />
-          </TimerBtn>
-        </TimerBtnContainer>
+        <Main
+          onClick={() =>
+            isMenuClick ? this.setState({ isMenuClick: false }) : ""
+          }
+          isMenu={isMenuClick}
+        >
+          <MenuConatiner>
+            <CurrentDoPanel {...this.state}>
+              <FontAwesomeIcon icon={faPen} style={{ marginRight: 10 }} />
+              Code
+            </CurrentDoPanel>
+            <CogBtn
+              onClick={() => this.setState({ isMenuClick: true })}
+              isMenu={isMenuClick}
+            >
+              <FontAwesomeIcon icon={faCog} />
+            </CogBtn>
+          </MenuConatiner>
+          <TimerContainer>
+            <DigitalTimer>25</DigitalTimer>
+            <DigitalTimer>
+              '<DigitalTimerDot>'</DigitalTimerDot>
+            </DigitalTimer>
+            <DigitalTimer>00</DigitalTimer>
+            <PlayerPreview>Soup Asmr - Tokyo Cafe Asmr</PlayerPreview>
+          </TimerContainer>
+          <TimerBtnContainer>
+            <TimerBtn>
+              <FontAwesomeIcon icon={faPlay} size="4x" color="white" />
+            </TimerBtn>
+            <TimerBtn>
+              <FontAwesomeIcon icon={faPause} size="4x" color="white" />
+            </TimerBtn>
+            <TimerBtn>
+              <FontAwesomeIcon icon={faStop} size="4x" color="white" />
+            </TimerBtn>
+          </TimerBtnContainer>
+        </Main>
+        {isMenuClick ? (
+          <Panel isCustom={isCustomClick}>
+            <ButtonColumn>
+              <ButtonConatiner>
+                <Title>Clock Mode</Title>
+                <SettingButton>Time Timer</SettingButton>
+                <SettingButton>Digital Timer</SettingButton>
+              </ButtonConatiner>
+              <ButtonConatiner>
+                <Title>Time Mode</Title>
+                <SettingButton
+                  onClick={() => this.setState({ isCustomClick: false })}
+                >
+                  Pomodoro
+                </SettingButton>
+                <SettingButton
+                  onClick={() => this.setState({ isCustomClick: true })}
+                >
+                  Custom
+                </SettingButton>
+              </ButtonConatiner>
+            </ButtonColumn>
+            <ButtonColumn>
+              <ButtonConatiner>
+                <Title>Timer Mode</Title>
+                <SettingButton>Timer Auto Start</SettingButton>
+                <SettingButton>Over Counting</SettingButton>
+              </ButtonConatiner>
+              <ButtonConatiner>
+                <Title>Time Setting</Title>
+                <SettingButton>
+                  Long Focus Time
+                  <TimeInput />
+                </SettingButton>
+                <SettingButton>
+                  Short Focus Time
+                  <TimeInput />
+                </SettingButton>
+                <SettingButton>
+                  Break Time
+                  <TimeInput />
+                  <SettingDescription> Break</SettingDescription>
+                </SettingButton>
+              </ButtonConatiner>
+            </ButtonColumn>
+            <ApplyButton>Apply</ApplyButton>
+          </Panel>
+        ) : (
+          ""
+        )}
       </Container>
     );
   }
 }
+
+const ApplyButton = styled.div`
+  position: absolute;
+  bottom: 15px;
+  left: 50%;
+  margin-left: -100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: #ff8f70;
+  color: white;
+`;
+
+const SettingDescription = styled.div`
+  top: 0;
+  ${props => (props.Left ? "left: 100px" : "right: 200px")}
+  position: absolute;
+  padding: 10px 20px;
+  border-radius: 25px;
+  background-color: white;
+`;
+
+const TimeInput = styled.input.attrs(props => ({ type: `number` }))`
+  width: 35px;
+  padding: 3px 0px;
+  border: none;
+  border-bottom: 1px solid #bdc3c7;
+  text-align: center;
+  font-weight: 300;
+  font-size: 15px;
+  color: black;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SettingButton = styled.h4`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  font-weight: 300;
+`;
+
+const Title = styled.h3`
+  font-weight: 400;
+`;
+
+const ButtonConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const ButtonColumn = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Panel = styled.div`
+  padding: 20px;
+  position: absolute;
+  top: 50%;
+  margin-top: -200px;
+  left: 50%;
+  margin-left: -150px;
+  width: 300px;
+  height: 450px;
+  border-radius: 25px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.27) 0 10px 20px;
+
+  /* transition: all 0.5s ease-in-out;
+  ${props => (props.isCustom ? "height: 400px;" : "height: 230px;")} */
+`;
 
 const CogBtn = styled.div`
   display: flex;
@@ -53,17 +199,11 @@ const CogBtn = styled.div`
   align-items: center;
   width: 50px;
   height: 50px;
-  color: rgba(255, 143, 112, 255);
+  color: ${props => (props.isMenu ? "#ff8f70" : "#bdc3c7")};
   font-size: 20px;
   background-color: white;
   border-radius: 25px;
   box-shadow: rgba(0, 0, 0, 0.27) 0 10px 20px;
-
-  /* transition: all 0.3s ease-in-out;
-  &:hover {
-    width: 100px;
-    transition: all 0.3s ease-in-out;
-  } */
 `;
 
 const CurrentDoPanel = styled.div`
@@ -78,7 +218,7 @@ const CurrentDoPanel = styled.div`
   align-items: center;
   font-size: 20px;
   font-weight: 600;
-  color: #ff8f70;
+  color: ${props => (props.isFocus ? "#ff8f70" : "#bdc3c7")};
 `;
 
 const MenuConatiner = styled.div`
@@ -87,7 +227,8 @@ const MenuConatiner = styled.div`
   align-items: center;
   position: absolute;
   top: 20px;
-  left: 47%;
+  left: 50%;
+  margin-left: -95px;
 `;
 
 const TimerBtnContainer = styled.div`
@@ -137,9 +278,19 @@ const TimerContainer = styled.div`
   position: relative;
 `;
 
-const Container = styled.div`
+const Main = styled.main`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  ${props => (props.isMenu ? "filter: blur(3px);" : "")}
+  ${props => (props.isMenu ? "-webkit-filter: blur(3px);" : "")}
+`;
+
+const Container = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
